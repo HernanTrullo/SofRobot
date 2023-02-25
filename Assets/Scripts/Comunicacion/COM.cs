bb4de793e6ce5486056a2153bcc6dcbecf5346d3
Hernan Dario Trullo <trullodario@unicauca.edu.co>
2023-02-24 19:44:51 -0500

    Modelo dinpamico inverso con parámetros reales

--- Assets/Scripts/Comunicacion/COM.cs
+++ Assets/Scripts/Comunicacion/COM.cs
@@ -2,24 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Uduino;
+using System.Threading;
+
 
 public class COM : MonoBehaviour
 {
+    public GameObject Uduino;
+
+    private UduinoManager uduinoManager;
     // Start is called before the first frame update
     void Start()
     {
-        UduinoManager.Instance.BaudRate = 115200;
-        UduinoManager.Instance.pinMode(13, PinMode.Output);
+        uduinoManager = Uduino.transform.GetComponent<UduinoManager>();
+        uduinoManager.pinMode(11, PinMode.PWM);
+        StartCoroutine(funcion());
     }
 
+    IEnumerator funcion(){
+        
+
+        for (int i=0; i<255; i++){
+
+            uduinoManager.analogWrite(11, i);
+            yield return new WaitForSeconds(0.1f); 
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)){
-            UduinoManager.Instance.digitalWrite(13, State.HIGH);
-        }
-        else if (Input.GetKeyUp(KeyCode.Space)){
-            UduinoManager.Instance.digitalWrite(13, State.LOW);
-        }
+        
     }
+
 }
